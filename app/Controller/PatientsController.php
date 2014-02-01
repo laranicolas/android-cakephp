cd586489207ec079af522a7c69cb4cda3fc4c903
Nicolás Lara <laranicolas@gmail.com>
2014-02-01 14:46:48 -0300

    Save response from sms patient.

--- app/Controller/PatientsController.php
+++ app/Controller/PatientsController.php
@@ -9,6 +9,10 @@ class PatientsController extends AppController {
 
 	public $helpers = array('Time');
 
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow('addResponse');
+	}
 /**
  * index method
  *
@@ -35,7 +39,8 @@ class PatientsController extends AppController {
 		$options = array(
 			'conditions' => array('Patient.' . $this->Patient->primaryKey => $id),
 			'contain' => array(
-				'CampaignsPatient' => array('Campaign')
+				'CampaignsPatient' => array('Campaign'),
+				'Sm'
 			)
 		);
 		$this->set('patient', $this->Patient->find('first', $options));
@@ -101,5 +106,65 @@ class PatientsController extends AppController {
 		}
 		$this->Session->setFlash(__d('Patient', 'Patient was not deleted'));
 		$this->redirect(array('action' => 'index'));
+	}
+
+/**
+ * Patients respond to app with sms from them cellulars.
+ * This method authenticate with patient and save information.
+ *
+ * @throws NotFoundException InternalErrorException
+ * @return void
+ */
+	public function addResponse() {
+		if (
+			!$this->request->is('get') ||
+			!isset($_SERVER['HTTP_PHONE']) || empty($this->request->query['text'])
+		) {
+			throw new InternalErrorException();
+		}
+
+		$smsPhone = ltrim($_SERVER['HTTP_PHONE'], '+549');
+		$smsText = $this->request->query['text'];
+		$this->log($smsPhone . $smsText);
+		if ($id = $this->__verifyPhone($smsPhone)) {
+
+			$this->Patient->id = $id;
+			if (!$this->Patient->exists()) {
+				throw new NotFoundException(__d('Patient', 'Invalid patient'));
+			}
+
+			$data['Sm'] = array(
+				'patient_id' => $id,
+				'text' => $smsText
+			);
+			$this->log($data);
+			if ($this->Patient->Sm->save($data)) {
+				return;
+			}
+		}
+
+		$this->log('Error incoming sms');
+	}
+
+/**
+ * Find if exist the sms phone came via http and return id from patient.
+ * In another case return false.
+ *
+ * @param int $smsPhone
+ * @return mixed boolean | int
+ */
+	private function __verifyPhone($smsPhone) {
+
+		$phones = hash::combine(
+			$this->Patient->find('all', array('fields' => array('code', 'cellular'))),
+			'{n}.Patient.id',
+			array('%s%s', '{n}.Patient.code', '{n}.Patient.cellular')
+		);
+		foreach ($phones as $id => $phone) {
+			if ($phone === $smsPhone) {
+				return $id;
+			}
+		}
+		return false;
 	}
 }
